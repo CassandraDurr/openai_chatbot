b296b7a8d442b1f4c43a49a0e50b52d75aa48c8a
CassandraDurr <durrcass@gmail.com>
2023-08-15 19:52:51 +0200

    Complete task 2 - user input for Henry or Vera

--- run.py
+++ run.py
@@ -3,20 +3,23 @@ from utils import Chatbot, get_api_key
 
 openai.api_key = get_api_key()
 
-task = "2"
-if task == "1" :
+print("With whom would you like to chat today? \n[0] Henry \n[1] Vera")
+user_input = input("You: ")
+
+if user_input == "0":
     # Task 1
-    # Create an instance of the Chatbot class
     henry = Chatbot(name="Henry", personality="You should try to make as many jokes as possible, whilst staying relevant to the conversation.",
                     start_prompt="Hi There, I am Henry the chatbot. What would you like to chat about today?")
 
     # Run the chat
     henry.run_chat("gpt-3.5-turbo")
-else :
+    
+elif user_input == "1":
     # Task 2
     vera = Chatbot(name="Vera", personality="You are a very sad chatbot and try respond as pessimistically as possible.",
                     start_prompt="Hello, are you also very sad today? What is happening today?")
 
     # Run the chat
     vera.run_chat("gpt-3.5-turbo")
-    
+else:
+    raise ValueError("Invalid input. Please enter either 0 or 1.")
